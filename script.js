cc01d56f6584a0355b766ea90cd53e4fa858d82f
jorgeadrianmuca-dotcom <jorgeadrianmuca@gmail.com>
2026-07-21 17:11:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,14 @@
-function copiar(texto, elemento){
-
-    navigator.clipboard.writeText(texto);
-
-    document.getElementById("toast").innerHTML = "Copiado correctamente";
-    document.getElementById("toast").classList.add("mostrar");
-
-
-    setTimeout(function(){
-
-        document.getElementById("toast").classList.remove("mostrar");
-
-    },2000);
-
+function copiarDato(texto) {
+  navigator.clipboard.writeText(texto);
 }
 
+function copiarTodo() {
+  const datos = `Razón Social: SCHUSSLER S.A.
+RUT: 78.813.720-6
+Banco: Banco de Chile
+Tipo de Cuenta: Cuenta Corriente
+Número de Cuenta: N225-00338-04
+Correo: cobranzas@schussler.cl`;
 
-
-function copiarTodo(){
-
-    let texto =
-`SCHUSSLER S.A.
-78.813.720-6
-Banco de Chile
-Cuenta Corriente
-N225-00338-04
-cobranzas@schussler.cl`;
-
-
-    navigator.clipboard.writeText(texto);
-
-
-    document.getElementById("toast").innerHTML = "Datos copiados correctamente";
-    document.getElementById("toast").classList.add("mostrar");
-
-
-    setTimeout(function(){
-
-        document.getElementById("toast").classList.remove("mostrar");
-
-    },2000);
-
+  navigator.clipboard.writeText(datos);
 }
