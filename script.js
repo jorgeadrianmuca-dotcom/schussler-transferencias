fa6bfeb426b33e435b411411290d8b8174136135
jorgeadrianmuca-dotcom <jorgeadrianmuca@gmail.com>
2026-07-21 17:28:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ let temporizador;
 function mostrarMensaje() {
   const mensaje = document.getElementById("mensaje");
   if (mensaje) {
+    // Hace visible el mensaje en el instante del clic
     mensaje.style.display = "block";
     
+    // Reinicia el temporizador si vuelves a hacer clic rápido
     clearTimeout(temporizador);
     
+    // Oculta el mensaje automáticamente tras 2 segundos
     temporizador = setTimeout(() => {
       mensaje.style.display = "none";
     }, 2000);
