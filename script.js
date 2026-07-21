3e11d90f2fb6aaef7d66b0745a19c1262e37957c
jorgeadrianmuca-dotcom <jorgeadrianmuca@gmail.com>
2026-07-21 17:16:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
+let temporizador;
+
+function mostrarMensaje() {
+  const mensaje = document.getElementById("mensaje");
+  if (mensaje) {
+    mensaje.style.display = "block";
+    
+    clearTimeout(temporizador);
+    
+    temporizador = setTimeout(() => {
+      mensaje.style.display = "none";
+    }, 2000);
+  }
+}
+
 function copiarDato(texto) {
   navigator.clipboard.writeText(texto);
+  mostrarMensaje();
 }
 
 function copiarTodo() {
-  const datos = `Razón Social: SCHUSSLER S.A.
-RUT: 78.813.720-6
-Banco: Banco de Chile
-Tipo de Cuenta: Cuenta Corriente
-Número de Cuenta: N225-00338-04
-Correo: cobranzas@schussler.cl`;
+  const datos = `SCHUSSLER S.A.
+78.813.720-6
+Banco de Chile
+Cuenta Corriente
+N225-00338-04
+cobranzas@schussler.cl`;
 
   navigator.clipboard.writeText(datos);
+  mostrarMensaje();
 }
